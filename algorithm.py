ec9d6a3cf857c98410c0077601511e0e60a5bdf8
PJ Oschmann <phillip236.po@gmail.com>
2024-10-01 20:42:28 -0400

    Create algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,41 @@
+def quickSort(a: list, l: int, r: int):
+    if (l < r):
+        s = hoarePartition(a[l:r+1])
+        quickSort(a, l, s-1)
+        quickSort(a, s+1, r)
+    
+    print(a)
+
+def hoarePartition(subA: list): 
+    """
+    Partitions the sublist.
+    subA: sub-list of the overall list. l = 0, r = n-1 of sublist.
+    """
+
+    # Get the left-most item in the list
+    p = subA[0]
+
+    i = 0 # Left-most element in the sublist
+    j = len(subA)-1 # Right-most element in the sublist
+
+    # while not x == repeat ... until x
+    while not (i >= j):
+        while not (subA[i] >= p):
+            i += 1
+        while not (subA[j] <= p):
+            j -= 1
+        _swap(subA, i, j)
+        print(f"hoare: {subA}")
+    
+    _swap(subA, i, j)
+    _swap(subA, 0, j)
+    return j
+
+def _swap(subA: list, idx1: int, idx2: int):
+    tmp = subA[idx1]
+    subA[idx1] = subA[idx2]
+    subA[idx2] = tmp
+
+myList = [9,2,-4,77]
+
+quickSort(myList, 0, len(myList)-1)
